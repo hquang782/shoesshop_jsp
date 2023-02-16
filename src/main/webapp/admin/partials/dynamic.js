93a6d955c42445b4ebff556bb8c643578bd4ae1d
minhquantt2002 <anhquankk51@gmail.com>
2023-02-16 13:55:40 +0700

    admin portal view

--- src/main/webapp/admin/partials/dynamic.js
+++ src/main/webapp/admin/partials/dynamic.js
@@ -1,5 +1,14 @@
 var dropdownAccount = document.getElementsByClassName('dropdown-account-link')[0];
+var btnAcountItem = document.getElementsByClassName('account-item')[0];
 
-function clickAccountItem(e) {
+btnAcountItem.addEventListener('click', () => {
     dropdownAccount.classList.contains('open-dropdown') ? dropdownAccount.classList.remove('open-dropdown') : dropdownAccount.classList.add('open-dropdown');
-}
+});
+
+
+var sidebar = document.getElementsByClassName('sidebar')[0];
+var btnSidebar = document.getElementsByClassName('btn-sidebar')[0];
+
+btnSidebar.addEventListener('click', () => {
+    sidebar.classList.contains('open-sidebar') ? sidebar.classList.remove('open-sidebar') : sidebar.classList.add('open-sidebar');
+});
--- src/main/webapp/user/partials/dynamic.js
+++ src/main/webapp/user/partials/dynamic.js
@@ -1,6 +1,6 @@
 
 window.onscroll = () => {
-    if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset > sticky) {
         nav.classList.add('sticky');
     } else {
         nav.classList.remove('sticky');
