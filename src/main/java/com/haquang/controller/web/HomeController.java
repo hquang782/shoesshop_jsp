1c72235c50fb95d36a68e250942ce22e5028d317
Ha Quang <haquang782002@gmail.com>
2023-04-06 10:44:17 +0700

    reinit

---
+++ src/main/java/com/haquang/controller/web/HomeController.java
@@ -0,0 +1,35 @@
+package com.haquang.controller.web;
+
+
+import com.haquang.model.UserModel;
+
+import java.io.IOException;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = {"/trang-chu"})
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserModel userModel = new UserModel();
+        userModel.setFullName("Hạ Đăng Quang");
+        req.setAttribute("model",userModel);
+        //        để trả về view
+
+        RequestDispatcher rd = req.getRequestDispatcher("/views/web/home.jsp");
+        rd.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
+
---
+++ src/main/webapp/views/admin/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Hạ Quang
+  Date: 10/02/2023
+  Time: 1:13 SA
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Trang chủ</title>
+</head>
+<body>
+<h1>đây là admin</h1>
+</body>
+</html>
